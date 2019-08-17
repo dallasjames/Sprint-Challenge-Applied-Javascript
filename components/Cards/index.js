90af67d9e8dfcf1b328e7d07044ce236616a8b27
Dallas James <dallasjames42@gmail.com>
2019-08-16 19:57:25 -0600

    got one card to work

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+function cardMaker(obj) {
+    let cards = document.querySelector('.cards-container')
+    for (let i = 0; i < obj.javascript.length; i++) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        
+        let headline = document.createElement('div');
+        headline.classList.add('headline');
+        headline.textContent = `${obj.javascript[i].headline}`;
+        cardDiv.appendChild(headline);
+        
+        let authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+        cardDiv.appendChild(authorDiv);
+        
+        let imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-container')
+        authorDiv.appendChild(imgDiv);
+        
+        let img = document.createElement('img');
+        img.setAttribute('src', `${obj.javascript[i].authorPhoto}`);
+        imgDiv.appendChild(img);
+        
+        let authorName = document.createElement('span');
+        authorName.textContet = `By ${obj.javascript[i].authorName}`;
+        authorDiv.appendChild(authorName);
+        
+        cards.appendChild(cardDiv);
+        
+        return cardDiv
+    }
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        console.log(response.data.articles);
+        document.querySelector('.cards-container').append(cardMaker(response.data.articles));
+    })
+    .catch((error) => {
+        console.log(error);
+    })
