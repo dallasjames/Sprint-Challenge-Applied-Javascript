8ca361a897105ad8da7c29c3946151bb220d8b39
Dallas James <dallasjames42@gmail.com>
2019-08-16 20:04:27 -0600

    done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -43,12 +43,118 @@ function cardMaker(obj) {
         imgDiv.appendChild(img);
         
         let authorName = document.createElement('span');
-        authorName.textContet = `By ${obj.javascript[i].authorName}`;
+        authorName.textContet = `${obj.javascript[i].authorName}`;
         authorDiv.appendChild(authorName);
         
-        cards.appendChild(cardDiv);
+        cards.appendChild(cardDiv);    
+    }
+    for (let i = 0; i < obj.bootstrap.length; i++) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
         
-        return cardDiv
+        let headline = document.createElement('div');
+        headline.classList.add('headline');
+        headline.textContent = `${obj.bootstrap[i].headline}`;
+        cardDiv.appendChild(headline);
+        
+        let authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+        cardDiv.appendChild(authorDiv);
+        
+        let imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-container')
+        authorDiv.appendChild(imgDiv);
+        
+        let img = document.createElement('img');
+        img.setAttribute('src', `${obj.bootstrap[i].authorPhoto}`);
+        imgDiv.appendChild(img);
+        
+        let authorName = document.createElement('span');
+        authorName.textContet = `${obj.bootstrap[i].authorName}`;
+        authorDiv.appendChild(authorName);
+        
+        cards.appendChild(cardDiv);    
+    }
+    for (let i = 0; i < obj.technology.length; i++) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        
+        let headline = document.createElement('div');
+        headline.classList.add('headline');
+        headline.textContent = `${obj.technology[i].headline}`;
+        cardDiv.appendChild(headline);
+        
+        let authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+        cardDiv.appendChild(authorDiv);
+        
+        let imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-container')
+        authorDiv.appendChild(imgDiv);
+        
+        let img = document.createElement('img');
+        img.setAttribute('src', `${obj.technology[i].authorPhoto}`);
+        imgDiv.appendChild(img);
+        
+        let authorName = document.createElement('span');
+        authorName.textContet = `${obj.technology[i].authorName}`;
+        authorDiv.appendChild(authorName);
+        
+        cards.appendChild(cardDiv);    
+    }
+    for (let i = 0; i < obj.jquery.length; i++) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        
+        let headline = document.createElement('div');
+        headline.classList.add('headline');
+        headline.textContent = `${obj.jquery[i].headline}`;
+        cardDiv.appendChild(headline);
+        
+        let authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+        cardDiv.appendChild(authorDiv);
+        
+        let imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-container')
+        authorDiv.appendChild(imgDiv);
+        
+        let img = document.createElement('img');
+        img.setAttribute('src', `${obj.jquery[i].authorPhoto}`);
+        imgDiv.appendChild(img);
+        
+        let authorName = document.createElement('span');
+        authorName.textContet = `${obj.jquery[i].authorName}`;
+        authorDiv.appendChild(authorName);
+        
+        cards.appendChild(cardDiv);    
+    }
+    for (let i = 0; i < obj.node.length; i++) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        
+        let headline = document.createElement('div');
+        headline.classList.add('headline');
+        headline.textContent = `${obj.node[i].headline}`;
+        cardDiv.appendChild(headline);
+        
+        let authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+        cardDiv.appendChild(authorDiv);
+        
+        let imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-container')
+        authorDiv.appendChild(imgDiv);
+        
+        let img = document.createElement('img');
+        img.setAttribute('src', `${obj.node[i].authorPhoto}`);
+        imgDiv.appendChild(img);
+        
+        let authorName = document.createElement('span');
+        authorName.textContet = `${obj.node[i].authorName}`;
+        authorDiv.appendChild(authorName);
+        
+        cards.appendChild(cardDiv);    
     }
 }
 
